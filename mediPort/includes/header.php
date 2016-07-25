be1e979b6a91fe0bca5c193f7a2605235130d3ea
Karan Kapoor <coffeewithkaran007@gmail.com>
2016-07-25 18:18:37 +0530

    Add files via upload

---
+++ mediPort/includes/header.php
@@ -0,0 +1,13 @@
+<header class="navbar navbar-inverse navbar-static-top" >
+			<div class="container">
+				<a href="index.php" class="navbar-brand">MyPracto</a>
+				<ul class="nav navbar-nav navbar-right">
+					<li><a href="index.php">Home</a></li>
+					<li><a href="about.php">About Us</a></li>
+					<li><a href="team.php">The Team</a></li>
+					<li><a href="contact.php">Contact Us</a></li>
+					<li><a href="registration.php">Registration</a></li>
+					<li><a href="bug.php">Report a Bug</a></li>
+				</ul>
+			</div>
+		</header>
---
+++ mediPort/includes/sidebar.php
@@ -0,0 +1,26 @@
+<aside class="col-lg-4">
+	<form class="panel-group form-horizontal" role="form" action="accounts/login.php" method="post">
+		<div class="panel panel-default">
+			<div class="panel-heading">Login Area for Admin</div>
+			<div class="panel-body">
+				<div class="form-group">
+					<label for="username" class="control-label col-sm-4">User Name</label>
+					<div class="col-sm-7">
+						<input type="text" id="username" placeholder="Insert Email Address" name="user_name" class="form-control">
+					</div>
+				</div>
+				<div class="form-group">
+					<label for="password" class="control-label col-sm-4">Password</label>
+					<div class="col-sm-7">
+						<input type="password" id="password" class="form-control" name="password" placeholder="Insert Your Password">
+					</div>
+				</div>
+				<div class="form-group">
+					<div class="col-sm-12">
+						<input type="submit" value="Submit" class="btn btn-success btn-block" name="submit_login">
+					</div>
+				</div>
+			</div>
+		</div>
+	</form>
+</aside>
